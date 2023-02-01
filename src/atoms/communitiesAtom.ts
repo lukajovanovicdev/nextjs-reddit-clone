3d4b611f6ab62e5222e93740da561e45edae9724
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2023-02-01 15:34:28 +0100

    Implement community page

---
+++ src/atoms/communitiesAtom.ts
@@ -0,0 +1,11 @@
+import { Timestamp } from 'firebase/firestore';
+import { atom } from 'recoil';
+
+export interface Community {
+  id: string;
+  creatorId: string;
+  numberOfMembers: number;
+  privacyType: 'public' | 'private' | 'restricted';
+  cratedAt?: Timestamp;
+  imageURL?: string;
+}
