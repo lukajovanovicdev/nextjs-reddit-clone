8333ff59284c7772393064f5d468cbe5495d10cf
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2023-02-01 15:07:11 +0100

    Create users document

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,21 @@
-import { auth } from '@/src/firebase/clientApp';
+import { auth, firestore } from '@/src/firebase/clientApp';
 import { Button, Flex, Image, Text } from '@chakra-ui/react';
-import React from 'react';
+import { User } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import React, { useEffect } from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const OAuthButtons: React.FC = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, userCred, loading, error] = useSignInWithGoogle(auth);
+
+  const createUserDocument = async (user: User) => {
+    const userDocRef = doc(firestore, 'users', user.uid);
+    await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));
+  };
+
+  useEffect(() => {
+    if (userCred) createUserDocument(userCred.user);
+  }, [userCred]);
   return (
     <Flex direction="column" mb={4} width="100%">
       <Button variant="oauth" mb={2} onClick={() => signInWithGoogle()} isLoading={loading}>
